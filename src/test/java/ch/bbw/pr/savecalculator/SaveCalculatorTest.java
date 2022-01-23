6c962e8f2ff29c4e4b393ee26a100d87feab0fe9
Melissa Wilkins <melissa.wilkins@espas.ch>
2022-01-23 18:15:26 +0100

    acht tests fur subtraktion wegen bewertung

--- src/test/java/ch/bbw/pr/savecalculator/SaveCalculatorTest.java
+++ src/test/java/ch/bbw/pr/savecalculator/SaveCalculatorTest.java
@@ -13,12 +13,18 @@ public class SaveCalculatorTest {
     SaveCalculator calculator;
     int max;
     int min;
+    int positiveZahl;
+    int negativeZahl;
+    int zero;
 
     @Before
     public void setup(){
         calculator = new SaveCalculator();
         max = Integer.MAX_VALUE;
         min = Integer.MIN_VALUE;
+        positiveZahl = 28;
+        negativeZahl = -42;
+        zero = 0;
 
     }
     @After
@@ -27,22 +33,28 @@ public class SaveCalculatorTest {
     }
 
     @Test
-    public void testSubtraktionMinRichtigeEingabe() throws Exception{
-        /* min Grenzwerte */
-
-        /* min - 0 */
-        int result1 = calculator.subtraktion(min, 0);
-        assertEquals(min, result1);
-        
-        /* min - -1 */
+    public void testSubtraktionMinRichtigeEingabe() 
+    {
+        /* min Grenzwerte */        
+        /* min - -1 = min + 1 ok */
         int result2 = calculator.subtraktion(min, -1);
         assertEquals(min + 1, result2);
     }
 
     @Test
+    public void testSubtraktionMinund0() 
+    {
+
+        /* min - 0 = min ok */
+        int result1 = calculator.subtraktion(min, 0);
+        assertEquals(min, result1);
+        
+    }
+
+    @Test
     public void testSubtraktionMinFalscheEingabe() throws Exception
     {
-        /* min - 1  */
+        /* min - 1 = overflow  */
         try {
             calculator.subtraktion(min, 1);
             fail();		
@@ -55,11 +67,14 @@ public class SaveCalculatorTest {
     public void testSubtraktionMaxRichtigeEingabe() 
     {
         /* max Grenzwerte */
-        /* max - 1 */
+        /* max - 1 = max -1 ok */
         int result3 = calculator.subtraktion(max, 1);
         assertEquals(max - 1, result3);
+    }
 
-        /* max - 0 */
+    public void testSubtraktionMaxund0() 
+    {
+        /* max - 0 = max ok */
         int result4 = calculator.subtraktion(max, 0);
         assertEquals(max, result4);
     }
@@ -67,7 +82,7 @@ public class SaveCalculatorTest {
     @Test
     public void testSubtraktionMaxFalscheEingabe() throws Exception
     {
-        /* 1 - max  */
+        /* 1 - max = overflow  */
         try {
             calculator.subtraktion(-2,  max);
             fail();		
@@ -75,16 +90,30 @@ public class SaveCalculatorTest {
             assertEquals("This calculation causes an overflow", e.getMessage());
             
         }
-
     }
 
+    @Test
+    public void testPostiveminusPositive(){
+        /* zwei positive zahlen subtrahieren */
+        int result = calculator.subtraktion(positiveZahl, positiveZahl);
+        assertEquals(0, result);
+        
+    }
+
+    @Test
+    public void testNegativeminusNegative(){
+        /* zwei positive zahlen subtrahieren */
+        int result = calculator.subtraktion(negativeZahl, negativeZahl);
+        assertEquals(0, result);
+        
+    }
     
     @Test
     public void testDivisionMit0(){
-        int zero = 0;
-        int zahl = 12;
+        
+        
         try {
-            calculator.division(zahl, zero);
+            calculator.division(12, zero);
             fail();
         } catch (ArithmeticException e) {
             assertEquals("Division with 0 causes an error", e.getMessage());
